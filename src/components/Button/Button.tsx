3de3f60078f6a19884c472da2d96ab96fd6fe9b7
Gabriel MBLABS <gabrielmblabs@NMB-DESENV-319.local>
2024-06-17 08:23:11 -0300

    touchableOpacityBoxProps

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import {Text} from '../Text/Text';
 import {useTheme} from '@shopify/restyle';
 import {Theme} from '../../theme/theme';
-import {Box} from '../Box/Box';
+import {Box, TouchableOpacityBoxProps} from '../Box/Box';
 
-interface ButtonProps {
+interface ButtonProps extends TouchableOpacityBoxProps {
   title: string;
   loading?: boolean;
 }
 
-export function Button({title, loading}: ButtonProps) {
+export function Button({
+  title,
+  loading,
+  ...touchableOpacityBoxProps
+}: ButtonProps) {
   const {colors} = useTheme<Theme>();
   return (
     <Box
@@ -18,7 +22,8 @@ export function Button({title, loading}: ButtonProps) {
       height={50}
       justifyContent="center"
       paddingHorizontal="s24"
-      alignItems="center">
+      alignItems="center"
+      {...touchableOpacityBoxProps}>
       <Text preset="paragraphMedium" bold>
         {title}
       </Text>
