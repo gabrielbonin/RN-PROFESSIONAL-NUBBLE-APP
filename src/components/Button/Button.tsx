c2685592cb7371d258f7a81628c4c15c8009d723
Gabriel MBLABS <gabrielmblabs@NMB-DESENV-319.local>
2024-06-23 13:35:11 -0300

    feat:Button Presets

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 
 import {Text} from '../Text/Text';
-import {useTheme} from '@shopify/restyle';
-import {Theme} from '../../theme/theme';
+
 import {TouchableOpacityBoxProps, TouchableOpacityBox} from '../Box/Box';
 import {ActivityIndicator} from 'react-native';
+import {buttonPresets} from './ButtonPresets';
+
+export type ButtonPreset = 'primary' | 'outline' | 'secondary';
 
 interface ButtonProps extends TouchableOpacityBoxProps {
   title: string;
   loading?: boolean;
+  preset?: ButtonPreset;
 }
 
 export function Button({
   title,
   loading,
+  preset = 'primary',
   ...touchableOpacityBoxProps
 }: ButtonProps) {
+  const buttonPreset = buttonPresets[preset];
   return (
     <TouchableOpacityBox
       backgroundColor="greenPrimary"
@@ -24,11 +29,12 @@ export function Button({
       alignItems="center"
       paddingHorizontal="s24"
       borderRadius="s16"
+      {...buttonPreset.container}
       {...touchableOpacityBoxProps}>
       {loading ? (
         <ActivityIndicator />
       ) : (
-        <Text preset="paragraphMedium" bold color="primaryContrast">
+        <Text preset="paragraphMedium" bold color={buttonPreset.content}>
           {title}
         </Text>
       )}
