3ad444bb5c3cac926d92810b0243538b0f3e268d
Gabriel MBLABS <gabrielmblabs@NMB-DESENV-319.local>
2024-06-30 19:20:43 -0300

    feat: login screen

--- App.tsx
+++ App.tsx
@@ -1,55 +1,12 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
-import {Text} from './src/components/Text/Text';
-
-import TextInput from './src/components/TextInput/TextInput';
 
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
-import {Box} from './src/components/Box/Box';
-import {Button} from './src/components/Button/Button';
-
-import {IconComponent} from './src/components/Icon/Icon';
-
+import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <View style={{paddingHorizontal: 24}}>
-          <Text marginBottom="s8" preset="headingLarge">
-            Olá!
-          </Text>
-          <Text marginBottom="s40" preset="paragraphLarge">
-            Digite seu e-mail e senha para entrar
-          </Text>
-          <TextInput
-            boxProps={{
-              marginBottom: 's20',
-            }}
-            label="Login"
-            placeholder="Digite seu e-mail"
-          />
-          <TextInput
-            boxProps={{
-              marginBottom: 's10',
-            }}
-            label="Senha"
-            errorMessage="Mensagem de Erro"
-            placeholder="Digite sua senha"
-            rightComponent={<IconComponent name="eyeOff" color="gray2" />}
-          />
-          <Text color="primary" bold preset="paragraphSmall">
-            Esqueci minha senha
-          </Text>
-          <Button title="Entrar" mt="s48" onPress={() => {}} />
-          <Button
-            preset="outline"
-            title="Criar uma conta"
-            mt="s12"
-            onPress={() => {}}
-          />
-        </View>
-      </SafeAreaView>
+      <LoginScreen />
     </ThemeProvider>
   );
 }
---
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {IconComponent} from '../../../components/Icon/Icon';
+import {Box} from '../../../components/Box/Box';
+import {Text} from '../../../components/Text/Text';
+import TextInput from '../../../components/TextInput/TextInput';
+import {Button} from '../../../components/Button/Button';
+
+export function LoginScreen() {
+  return (
+    <SafeAreaView>
+      <Box style={{paddingHorizontal: 24}}>
+        <Text marginBottom="s8" preset="headingLarge">
+          Olá!
+        </Text>
+        <Text marginBottom="s40" preset="paragraphLarge">
+          Digite seu e-mail e senha para entrar
+        </Text>
+        <TextInput
+          boxProps={{
+            marginBottom: 's20',
+          }}
+          label="Login"
+          placeholder="Digite seu e-mail"
+        />
+        <TextInput
+          boxProps={{
+            marginBottom: 's10',
+          }}
+          label="Senha"
+          errorMessage="Mensagem de Erro"
+          placeholder="Digite sua senha"
+          rightComponent={<IconComponent name="eyeOff" color="gray2" />}
+        />
+        <Text color="primary" bold preset="paragraphSmall">
+          Esqueci minha senha
+        </Text>
+        <Button title="Entrar" mt="s48" onPress={() => {}} />
+        <Button
+          preset="outline"
+          title="Criar uma conta"
+          mt="s12"
+          onPress={() => {}}
+        />
+      </Box>
+    </SafeAreaView>
+  );
+}
