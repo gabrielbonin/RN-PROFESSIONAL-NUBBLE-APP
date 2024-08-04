b09a69731a913c69b02aa67feb4f5bcfa3131cb9
Bonin <bonin@MBP-de-Bonin.lan>
2024-08-03 23:31:59 -0300

    ios run and postApi

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -2,8 +2,20 @@ import {postListMock} from './postListMock';
 import {Post} from './types';
 
 async function getList(): Promise<Post[]> {
-  //simulate network request
-  await new Promise(resolve => setTimeout(resolve, 3000));
+  let headersList = {
+    Authorization:
+      'Bearer Mw.2AtD-I0kv2ZWVmyN9fdMAPancS7r3oFpS-j8JDtKd1F1mb-pz2Bp7eBqIjBP',
+  };
+
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  console.log('FETCH: ', data);
+
+  // await new Promise(resolve => setTimeout(resolve, 3000));
 
   return postListMock;
 }
